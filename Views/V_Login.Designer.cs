51821b8ecf599666ef45feaf9d25e0052e4fb25a
sapta <saptamabar@gmail.com>
2024-11-12 23:45:12 +0700

    Add project files.

---
+++ Views/V_Login.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace PBO_B1
+{
+    partial class V_Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(V_Login));
+            tbUsername = new TextBox();
+            tbPassword = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // tbUsername
+            // 
+            tbUsername.BorderStyle = BorderStyle.None;
+            tbUsername.Location = new Point(507, 219);
+            tbUsername.Name = "tbUsername";
+            tbUsername.Size = new Size(349, 20);
+            tbUsername.TabIndex = 0;
+            tbUsername.TextChanged += tbUsername_TextChanged;
+            // 
+            // tbPassword
+            // 
+            tbPassword.BorderStyle = BorderStyle.None;
+            tbPassword.Location = new Point(507, 294);
+            tbPassword.Name = "tbPassword";
+            tbPassword.PasswordChar = '*';
+            tbPassword.Size = new Size(349, 20);
+            tbPassword.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(630, 385);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(97, 29);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "LOGIN";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += this.btnLogin_Click;
+            // 
+            // V_Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(942, 493);
+            Controls.Add(btnLogin);
+            Controls.Add(tbPassword);
+            Controls.Add(tbUsername);
+            DoubleBuffered = true;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "V_Login";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+
+        #endregion
+
+        private TextBox tbUsername;
+        private TextBox tbPassword;
+        private Button btnLogin;
+    }
+}
