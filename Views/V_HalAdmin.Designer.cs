02b33122daff9cd23155e9a9798ee67e28952a53
sapta <saptamabar@gmail.com>
2024-11-25 13:59:07 +0700

    adding datagridview

--- Views/V_HalAdmin.Designer.cs
+++ Views/V_HalAdmin.Designer.cs
@@ -28,32 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            btnTambahAdmin = new Button();
+            dataGridAdmin = new DataGridView();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)dataGridAdmin).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
-            // label1
+            // btnTambahAdmin
             // 
-            label1.BackColor = SystemColors.ActiveBorder;
-            label1.Dock = DockStyle.Top;
-            label1.Location = new Point(0, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(749, 184);
-            label1.TabIndex = 0;
-            label1.Text = "Admin";
-            label1.TextAlign = ContentAlignment.MiddleCenter;
+            btnTambahAdmin.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnTambahAdmin.ForeColor = Color.Black;
+            btnTambahAdmin.Location = new Point(980, 78);
+            btnTambahAdmin.Name = "btnTambahAdmin";
+            btnTambahAdmin.Size = new Size(110, 56);
+            btnTambahAdmin.TabIndex = 0;
+            btnTambahAdmin.Text = "Tambah";
+            btnTambahAdmin.UseVisualStyleBackColor = true;
+            // 
+            // dataGridAdmin
+            // 
+            dataGridViewCellStyle1.ForeColor = Color.Black;
+            dataGridAdmin.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridAdmin.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridAdmin.BackgroundColor = Color.White;
+            dataGridAdmin.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridAdmin.Dock = DockStyle.Fill;
+            dataGridAdmin.GridColor = Color.Black;
+            dataGridAdmin.Location = new Point(3, 165);
+            dataGridAdmin.Name = "dataGridAdmin";
+            dataGridAdmin.RowHeadersWidth = 51;
+            dataGridAdmin.Size = new Size(1146, 642);
+            dataGridAdmin.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.BackColor = Color.FromArgb(255, 128, 0);
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(dataGridAdmin, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 80F));
+            tableLayoutPanel1.Size = new Size(1152, 810);
+            tableLayoutPanel1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btnTambahAdmin);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1146, 156);
+            panel1.TabIndex = 2;
             // 
             // V_HalAdmin
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(label1);
+            Controls.Add(tableLayoutPanel1);
             Name = "V_HalAdmin";
-            Size = new Size(749, 460);
+            Size = new Size(1152, 810);
+            ((System.ComponentModel.ISupportInitialize)dataGridAdmin).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Label label1;
+        private Button btnTambahAdmin;
+        private DataGridView dataGridAdmin;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Panel panel1;
     }
 }
--- Views/V_HalAdmin.cs
+++ Views/V_HalAdmin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PBO_B1.App.Context;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,67 @@ namespace PBO_B1.Views
         public V_HalAdmin()
         {
             InitializeComponent();
+            Loaddatagridview();
+
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void Loaddatagridview()
+        {
+            dataGridAdmin.ReadOnly = true;
+            dataGridAdmin.AllowUserToAddRows = false;
+            DataTable data_admin = C_Admin.Admin();
+            if (data_admin == null)
+            {
+                MessageBox.Show("Error gagal memuat data");
+                return;
+            }
+
+            dataGridAdmin.Columns.Clear();
+
+            DataGridViewTextBoxColumn NomorKolom = new DataGridViewTextBoxColumn();
+            NomorKolom.HeaderText = "No";
+            NomorKolom.Name = "Nomor";
+            dataGridAdmin.Columns.Add(NomorKolom);
+
+
+            dataGridAdmin.DataSource = data_admin;
+
+            if (dataGridAdmin.Columns["akun_id"] != null)
+                dataGridAdmin.Columns["akun_id"].HeaderText = "ID";
+
+
+            for (int i = 0; i < dataGridAdmin.Rows.Count; i++)
+            {
+                dataGridAdmin.Rows[i].Cells["Nomor"].Value = (i + 1).ToString();
+            }
+
+            DataGridViewButtonColumn updateButtonColumn = new DataGridViewButtonColumn()
+            {
+                Name = "Update",
+                HeaderText = "Update",
+                Text = "Edit",
+                UseColumnTextForButtonValue = true,
+            };
+
+            DataGridViewButtonColumn deleteButtonColumn = new DataGridViewButtonColumn()
+            {
+                Name = "Delete",
+                HeaderText = "Delete",
+                Text = "Delete",
+                UseColumnTextForButtonValue = true,
+            };
+
+            dataGridAdmin.Columns.Add(updateButtonColumn);
+            dataGridAdmin.Columns.Add(deleteButtonColumn);
+
+
+        }
+
+        
     }
+
 }
