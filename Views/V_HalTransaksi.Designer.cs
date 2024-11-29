febe38722d1e1fb0a47815c74598f9542b6600a2
Luqman Adhikara <laa22072005@gmail.com>
2024-11-29 07:02:49 +0700

    melihat data transaksi (klik labelnya dulu)

--- Views/V_HalTransaksi.Designer.cs
+++ Views/V_HalTransaksi.Designer.cs
@@ -2,35 +2,13 @@
 {
     partial class V_HalTransaksi
     {
-        /// <summary> 
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Component Designer generated code
-
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             label1 = new Label();
-            dataGridView2 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            dataGridViewTransaksi = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewTransaksi).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -46,33 +24,32 @@
             label1.TextAlign = ContentAlignment.MiddleCenter;
             label1.Click += label1_Click;
             // 
-            // dataGridView2
+            // dataGridViewTransaksi
             // 
-            dataGridView2.AllowUserToOrderColumns = true;
-            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView2.Dock = DockStyle.Fill;
-            dataGridView2.Location = new Point(0, 166);
-            dataGridView2.Name = "dataGridView2";
-            dataGridView2.RowHeadersWidth = 82;
-            dataGridView2.Size = new Size(1688, 658);
-            dataGridView2.TabIndex = 2;
+            dataGridViewTransaksi.AllowUserToOrderColumns = true;
+            dataGridViewTransaksi.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewTransaksi.Dock = DockStyle.Fill;
+            dataGridViewTransaksi.Location = new Point(0, 166);
+            dataGridViewTransaksi.Name = "dataGridViewTransaksi";
+            dataGridViewTransaksi.RowHeadersWidth = 82;
+            dataGridViewTransaksi.Size = new Size(1688, 658);
+            dataGridViewTransaksi.TabIndex = 2;
+            dataGridViewTransaksi.CellContentClick += dataGridViewTransaksi_CellContentClick; // Perbaiki nama event handler
             // 
             // V_HalTransaksi
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(dataGridView2);
+            Controls.Add(dataGridViewTransaksi);
             Controls.Add(label1);
             Margin = new Padding(5);
             Name = "V_HalTransaksi";
             Size = new Size(1688, 824);
-            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewTransaksi).EndInit();
             ResumeLayout(false);
         }
 
-        #endregion
-
         private Label label1;
-        private DataGridView dataGridView2;
+        private DataGridView dataGridViewTransaksi;
     }
 }
--- Views/V_HalTransaksi.cs
+++ Views/V_HalTransaksi.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using PBO_B1.App.Context; // Menggunakan C_Transaksi untuk mengambil data transaksi
+using System;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PBO_B1.Views
@@ -17,9 +12,53 @@ namespace PBO_B1.Views
             InitializeComponent();
         }
 
+        private void V_HalTransaksi_Load(object sender, EventArgs e)
+        {
+            LoadDataTransaksi(); // Memuat data saat form dimuat
+        }
+
+        // Method untuk memuat data ke DataGridView
+        private void LoadDataTransaksi()
+        {
+            try
+            {
+                // Ambil data dari C_Transaksi
+                DataTable dataTransaksi = C_Transaksi.All();
+
+                // Debugging: Cek apakah data ada
+                if (dataTransaksi == null || dataTransaksi.Rows.Count == 0)
+                {
+                    MessageBox.Show("Tidak ada data transaksi ditemukan.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                // Debugging: Log jumlah data yang diterima
+                Console.WriteLine($"Jumlah data transaksi: {dataTransaksi.Rows.Count}");
+
+                // Isi DataGridView dengan data
+                dataGridViewTransaksi.DataSource = dataTransaksi;
+
+                // (Opsional) Atur properti DataGridView
+                dataGridViewTransaksi.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dataGridViewTransaksi.ReadOnly = true; // Data hanya untuk ditampilkan
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Gagal memuat data transaksi: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Event handler untuk klik pada label1
         private void label1_Click(object sender, EventArgs e)
         {
+            // Ketika label1 diklik, refresh DataGridView dengan memuat ulang data
+            LoadDataTransaksi(); // Memanggil LoadDataTransaksi untuk mengupdate data
+        }
 
+        // Event handler untuk klik pada DataGridView
+        private void dataGridViewTransaksi_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Anda bisa tambahkan logika di sini jika ingin aksi khusus saat user klik cell.
         }
     }
 }
