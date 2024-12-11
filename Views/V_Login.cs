62b43d43a95fa26315ebfbb92471b199e1b46eff
sapta <saptamabar@gmail.com>
2024-12-11 22:27:49 +0700

    Add Loading Screen

--- Views/V_Login.cs
+++ Views/V_Login.cs
@@ -37,6 +37,8 @@ namespace PBO_B1
             {
                 
                 Hide();
+                V_Loadingscreen loadingscreen = new V_Loadingscreen();
+                loadingscreen.ShowDialog();
                 V_HalUtamaPemilik Halutama = new V_HalUtamaPemilik();
                 Halutama.ShowDialog();
                 
